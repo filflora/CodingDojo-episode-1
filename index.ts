62ee4085b4739f2f6b13b59f1694cc532ec4521c
Fil <whirlion@gmail.com>
2019-10-10 07:52:05 +0200

    Added missing file from previous commit

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
-const RomanNumberCalculator = require('./RomanNumberCalculator.js');
-const _ = require('lodash');
-const fs = require('fs');
+import { RomanNumberCalculator } from './RomanNumberCalculator';
+import { readFileSync } from 'fs';
 
-const banner = fs.readFileSync('./resources/banner.txt', 'utf8');
+const banner = readFileSync('./resources/banner.txt', 'utf8');
 const calculator = new RomanNumberCalculator();
-const inputArr = calculator.parseFile("./resources/lines.txt");
-const outputArr = calculator.parseFile("./resources/result.txt");
-const maxNumberCount = inputArr.reduce((max, input) => Math.max(max, input.length), 0);
+const inputArr: string[][] = calculator.parseFile("./resources/lines.txt");
+const outputArr: string[][] = calculator.parseFile("./resources/result.txt");
+const maxNumberCount: number = inputArr.reduce((max, input) => Math.max(max, input.length), 0);
 
 console.log(banner);
 
 // Calculate results
 const results = inputArr
-    .map((numbers, index) => {
+    .map((numbers: string[], index) => {
         const numberObj = Array.apply(null, Array(maxNumberCount))
-            .map((value, index) => index)
+            .map((_value, index) => index)
             .reduce((acc, index) => {
                 acc['number' + (index + 1)] = numbers[index] || null;
                 return acc;
-            }, {});
+            }, {} as { [key: string]: string | null});
 
         return {
             ...numberObj,
+            // @ts-ignore
             calculatedResult: calculator.solve(...numbers),
             expectedResult: outputArr[index][0]
         }
